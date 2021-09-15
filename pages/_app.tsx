cff75ac39ecf176a0b5b73303beb8e3a115e2d76
Ken Kauksi <kensservices@gmail.com>
2021-09-15 22:36:19 +0300

    Add stuff for lighthouse

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import 'tailwindcss/tailwind.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <title>Visioncraft secret lair</title>
+    <meta name="description" content="Test assignment for visioncraft" />
+  </Head>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
